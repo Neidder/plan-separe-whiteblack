af0b1e7f767507f9e22f5ecba8e718a962b7a9d5
Jhojan17692 <jhojan_vasquez@elpoli.edu.co>
2026-02-25 11:07:18 -0500

    base

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,20 +1,18 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-from dotenv import load_dotenv
-import os
+from sqlalchemy.orm import declarative_base, sessionmaker
 
-load_dotenv()
-
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = "mysql+pymysql://root:@localhost:3306/plan_separe"
 
 engine = create_engine(DATABASE_URL)
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
 
 Base = declarative_base()
 
-# Dependencia para obtener la sesión de BD en cada endpoint
 def get_db():
     db = SessionLocal()
     try:
