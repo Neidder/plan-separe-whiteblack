060acbd8c27a105ab2a4ba8e1fdba009ba2bd90f
Neidder <neider_blandon69232@elpoli.edu.co>
2026-03-26 10:25:57 -0500

    avance dashboard y mejoras UI

--- frontend/src/pages/Clientes.jsx
+++ frontend/src/pages/Clientes.jsx
@@ -1,4 +1,18 @@
+import { useState, useEffect } from 'react';
+import Sidebar from '../components/Sidebar';
+import { getClientes, crearCliente, actualizarCliente, eliminarCliente  } from '../api/clientes';
+
 const Clientes = () => {
-    return <div><h1>Clientes</h1></div>;
-};
-export default Clientes;
+    const [clientes, setClientes] = useState([]);
+    const [formData, setFormData] = useState({ nombre: '', email: '' });
+    const [editingId, setEditingId] = useState(null);
+    
+
+    useEffect(() => {      
+    fetchClientes();
+    }, []);
+
+    const fetchClientes = async () => {
+        const data = await getClientes();
+        setClientes(data);
+    };
