1de954e5b59d0f22608df62a558245b1752e6b60
Neidder <neider_blandon69232@elpoli.edu.co>
2026-03-26 16:16:47 -0500

    avance dashboard y mejoras UI

--- frontend/src/api/clientes.js
+++ frontend/src/api/clientes.js
@@ -1,23 +1,2 @@
-import api from './axios';
-
-export const getClientes = async () => {
-    const response = await api.get('/clientes/clientes/');
-    return response.data;
-};
-
-export const crearCliente = async (data) => {
-    const response = await api.post('/clientes/clientes/', data);
-    return response.data;
-};
-
-export const actualizarCliente = async (id, data) => {
-    const response = await api.put(`/clientes/clientes/${id}/`, data);
-    return response.data;
-};
-
-export const eliminarCliente = async (id) => {
-    const response = await api.delete(`/clientes/clientes/${id}/`);
-    return response.data;
-};
 
 
